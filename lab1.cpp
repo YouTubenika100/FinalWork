cddb523eded5df94853502c882df921fcd739a4e
Nikita <ya.nika100@gmail.com>
2025-12-24 16:41:32 +0300

    Студент №1: переименовал переменную, добавил отчёт

---
+++ lab1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a1, b, sum;
+
+    a = 10;
+    b = 5;
+
+    sum = a + b;
+
+    cout << "Sum = " << sum << endl;
+
+    return 0;
+}
